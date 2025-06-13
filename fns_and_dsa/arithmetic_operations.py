ecb6016db6f3ce0c8ad05cc16fca453217ddf08c
Oluwaseyi Fashina <73977695+sheyifash@users.noreply.github.com>
2025-06-13 21:53:17 +0100

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 == 0
+        if num2 == 0:
             return "DIVISION_BY_ZERO_ERROR"
         return num1 / num2
     else:
